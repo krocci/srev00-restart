9dfde843b0d2d39afeaa2c82ba745020fd736577
krocci <104976051+krocci@users.noreply.github.com>
2024-09-21 17:06:59 +0800

    Update list.sh

--- list.sh
+++ list.sh
@@ -4,7 +4,7 @@
 IFS=';' read -r -a ACCOUNTS_array <<< "$ACCOUNTS"
 
 # 创建一个临时文件来存储所有的 list.txt 内容
-#TEMP_FILE=$(mktemp)
+TEMP_FILE=$(mktemp)
 
 # 初始化计数器
 success_count=0
@@ -33,7 +33,7 @@ EOF
     failure_count=$((failure_count + 1))
     failed_users+=("$USERNAME@$SERVER")
   fi
-done #>> $TEMP_FILE
+done >> $TEMP_FILE
 
 #发送通知到 Telegram
 echo "发送登录结果"
@@ -48,10 +48,10 @@ fi
 curl -s -X POST https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage -d chat_id=$TELEGRAM_USER_ID -d text="$message"
 
 #发送节点信息
-#grep -E '(vmess|hysteria2|tuic)://' $TEMP_FILE > list.txt
+grep -E '(vmess|hysteria2|tuic)://' $TEMP_FILE > list.txt
 
 # 上传 list.txt
 curl -s -X POST https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendDocument -F chat_id=$TELEGRAM_USER_ID -F document=@list.txt
 # 删除临时文件
-#rm $TEMP_FILE
-#rm list.txt
+rm $TEMP_FILE
+rm list.txt
